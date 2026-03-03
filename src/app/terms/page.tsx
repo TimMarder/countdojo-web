a8a0c3c94be958695231fbd361467b4df363ed1f
Tim Marder <timmarder@gmail.com>
2026-03-03 20:04:59 +0000

    Logo 60px on privacy/terms

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -14,7 +14,7 @@ export default function TermsPage() {
                 alt="Count Dojo"
                 width={200}
                 height={40}
-                className="h-[150px] w-auto object-cover"
+                className="h-[60px] w-auto object-contain"
                 priority
               />
             </Link>
